ce85ab80b03bb1c13bb278c15b8e923656ba04f7
petayyyy <47917455+petayyyy@users.noreply.github.com>
2023-05-31 19:55:49 +0300

    Update install_all_dependence.sh

--- build/install_all_dependence.sh
+++ build/install_all_dependence.sh
@@ -7,6 +7,10 @@ wget -O - apt.radxa.com/focal-stable/public.key | sudo apt-key add -
 sudo apt update
 sudo apt-get install -y rockchip-overlay
 sudo apt-get install -y broadcom-wifibt-firmware
+sudo apt install libsndfile1-dev -y
+sudo apt-get install libsoapysdr-dev -y
+sudo apt install libspdlog-dev -y
+sudo apt-get -y install python3-pip -y
 
 echo "Build Realsense"
 bash ./installing_realsense.sh
