1b46816f79ab96f9861d7dd25c30536febacc0be
petayyyy <47917455+petayyyy@users.noreply.github.com>
2023-05-31 19:41:25 +0300

    Update installing_ros.sh

--- build/installing_ros.sh
+++ build/installing_ros.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "Build ROS noetic"
-
 echo "Set up your keys"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl # if you haven't already installed curl
@@ -39,3 +37,20 @@ source ~/.bashrc
 
 echo "Check ROS path"
 echo $ROS_PACKAGE_PATH
+
+echo "Install Mavros"
+sudo apt-get -y install ros-noetic-mavros*
+cd /opt/ros/noetic/lib/mavros
+sudo ./install_geographiclib_datasets.sh
+sudo apt install -y ros-noetic-ddynamic-reconfigure*
+
+echo "Install C4S_v2 ros packages"
+sudo apt install -y git
+cd ~
+if [ ! -f  "/Lorett" ]; then
+    git clone https://github.com/petayyyy/Lorett.git
+fi
+cd Lorett
+mv packages/* ~/catkin_ws/src
+cd ~/catkin_ws
+catkin_make
