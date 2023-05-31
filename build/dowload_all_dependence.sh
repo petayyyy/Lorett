06701a52d83de8031b4eaf980d1dc0c556e7bc6d
petayyyy <47917455+petayyyy@users.noreply.github.com>
2023-05-31 18:03:15 +0300

    Update dowload_all_dependence.sh
    Clear download sh

--- build/dowload_all_dependence.sh
+++ build/dowload_all_dependence.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd ~/
 echo "Create path for all dependence and libs"
 mkdir Dependence && cd Dependence
 path=`pwd`
@@ -8,52 +9,16 @@ echo "Preparation before installing packages and libs"
 sudo apt update
 
 echo "Downloading all packages and libs for sdr"
-sudo apt download -y --fix-missing airspy
-sudo apt download -y --fix-missing soapysdr-module-airspy
-sudo apt download -y --fix-missing soapysdr-tools
-sudo apt download -y --fix-missing libairspy-dev 
+sudo apt download -y airspy soapysdr-module-airspy soapysdr-tools libairspy-dev 
 
-echo "Download libs for python 2 and 3 for sdr"
-sudo apt download -y --fix-missing python-scipy
-sudo apt download -y --fix-missing python-matplotlib
-sudo apt download -y --fix-missing python-soapysdr
-sudo apt download -y --fix-missing python-numpy
-sudo apt download -y --fix-missing python-mako
-sudo apt download -y --fix-missing python-pyorbital
-sudo apt download -y --fix-missing python-docopt
-
-sudo apt download -y --fix-missing python3-scipy
-sudo apt download -y --fix-missing python3-matplotlib
-sudo apt download -y --fix-missing python3-soapysdr
-sudo apt download -y --fix-missing python3-numpy
-sudo apt download -y --fix-missing python3-mako
-sudo apt download -y --fix-missing python3-pyorbital
-sudo apt download -y --fix-missing python3-docopt
+echo "Download libs for python 3 for sdr"
+sudo apt download -y python3-scipy python3-matplotlib python3-soapysdr python3-numpy python3-mako python3-pyorbital python3-docopt
 
 echo "Download all packeges and libs for SatDump"
-sudo apt download -y --fix-missing git
-sudo apt-get install -y --fix-missing git
-sudo apt download -y --fix-missing build-essential
-sudo apt download -y --fix-missing cmake
-sudo apt download -y --fix-missing g++
-sudo apt download -y --fix-missing pkgconf
-sudo apt download -y --fix-missing libfftw3-dev
-sudo apt download -y --fix-missing libvolk2-dev
-sudo apt download -y --fix-missing libjpeg-dev
-sudo apt download -y --fix-missing libpng-dev
-sudo apt download -y --fix-missing librtlsdr-dev
-sudo apt download -y --fix-missing libhackrf-dev
-sudo apt download -y --fix-missing libairspy-dev
-sudo apt download -y --fix-missing libairspyhf-dev                          
-sudo apt download -y --fix-missing libglew-dev
-sudo apt download -y --fix-missing libglfw3-dev   
-sudo apt download -y --fix-missing libzstd-dev   
-sudo apt download -y --fix-missing xorg-dev
-sudo apt download -y --fix-missing libglu1-mesa-dev
-sudo apt download -y --fix-missing freeglut3-dev
-sudo apt download -y --fix-missing mesa-common-dev       
+sudo apt download -y git build-essential cmake g++ pkgconf libfftw3-dev libvolk2-dev libjpeg-dev libpng-dev librtlsdr-dev libhackrf-dev libairspy-dev libairspyhf-dev libglew-dev libglfw3-dev libzstd-dev xorg-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev  
 
 echo "Download all git repositories"
+sudo apt-get install -y git
 git clone https://github.com/pothosware/SoapySDR.git
 git clone https://github.com/pothosware/SoapyAirspy.git
 git clone https://github.com/nanomsg/nng.git
