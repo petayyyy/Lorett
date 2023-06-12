fb9b469fa3bafbaa4f92aa6cbe4d1b72f3abb3de
petayyyy <47917455+petayyyy@users.noreply.github.com>
2023-06-13 01:07:46 +0300

    Update installing_ros.sh

--- build/installing_ros.sh
+++ build/installing_ros.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Set up your keys"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl # if you haven't already installed curl
+sudo apt install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 echo "Install ROS Desktop"
 sudo apt update
-sudo apt install ros-noetic-desktop-full
+sudo apt install -y ros-noetic-desktop-full
 
 echo "Check available packages"
 apt search ros-noetic
