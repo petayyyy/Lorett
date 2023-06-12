bd201547e825731b970144916056b6854d9d2e1e
petayyyy <47917455+petayyyy@users.noreply.github.com>
2023-06-13 01:02:13 +0300

    Update install_all_dependence.sh

--- build/install_all_dependence.sh
+++ build/install_all_dependence.sh
@@ -3,12 +3,12 @@ path="~/Lorett/build"
 
 echo "Build Rock dependence"
 sudo apt install -y wget
-wget -O - apt.radxa.com/focal-stable/public.key | sudo apt-key add -
 sudo apt update
-sudo apt-get install -y rockchip-overlay
-sudo apt-get install -y broadcom-wifibt-firmware
+
+
+
 sudo apt install libsndfile1-dev -y
-sudo apt-get install libsoapysdr-dev -y
+#sudo apt-get install libsoapysdr-dev -y
 sudo apt install libspdlog-dev -y
 sudo apt-get -y install python3-pip -y
 sudo apt-get -y install bsdmainutils
